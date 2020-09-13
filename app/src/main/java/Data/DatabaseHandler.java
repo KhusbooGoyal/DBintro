e3b57ecd8f823b3f9a399a267fe8ca3a0af8e988
Khusboo <khusboo1299@gmail.com>
2020-09-13 23:18:02 +0530

    Second commit

--- app/src/main/java/Data/DatabaseHandler.java
+++ app/src/main/java/Data/DatabaseHandler.java
@@ -4,21 +4,34 @@ import android.content.Context;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 
-import androidx.annotation.Nullable;
+import Utils.Util;
+
+//import androidx.annotation.Nullable;
 
 public class DatabaseHandler extends SQLiteOpenHelper {
 
-    public DatabaseHandler(@Nullable Context context, @Nullable String name, @Nullable SQLiteDatabase.CursorFactory factory, int version) {
-        super(context, name, factory, version);
+    public DatabaseHandler(Context context) {
+        super(context, Util.DATABASE_NAME, null, Util.DATABASE_VERSION);
+
     }
 
+    //create table
     @Override
     public void onCreate(SQLiteDatabase db) {
+        //SQL = Structure Query Language
+        String CREATE_CONTACT_TABLE = "CREATE TABLE "+ Util.TABLE_NAME + "("
+                + Util.KEY_ID + " INTEGER PRIMARY KEY," + Util.KEY_NAME + "TEXT,"
+                + Util.KEY_PHONE_NUMBER + " TEXT" + ")";
 
+        db.execSQL(CREATE_CONTACT_TABLE);
     }
 
     @Override
     public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
 
+        db.execSQL("DROP TABLE IF EXISTS " + Util.TABLE_NAME);
+
+        //CREATE TABLE AGAIN
+        onCreate(db);
     }
 }
