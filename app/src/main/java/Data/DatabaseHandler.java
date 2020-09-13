cb678afabf3188b0b5ecbc69628145212e145a1d
Khusboo <khusboo1299@gmail.com>
2020-09-13 23:33:22 +0530

    Third commit

--- app/src/main/java/Data/DatabaseHandler.java
+++ app/src/main/java/Data/DatabaseHandler.java
@@ -1,9 +1,12 @@
 package Data;
 
+import android.content.ContentValues;
 import android.content.Context;
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 
+import Model.Contact;
 import Utils.Util;
 
 //import androidx.annotation.Nullable;
@@ -33,5 +36,34 @@ public class DatabaseHandler extends SQLiteOpenHelper {
 
         //CREATE TABLE AGAIN
         onCreate(db);
+    }
+
+    //ADD CONTACT
+    public void addContact(Contact contact){
+        SQLiteDatabase db = this.getWritableDatabase();
+
+        ContentValues value = new ContentValues();
+        value.put(Util.KEY_NAME, contact.getName());
+        value.put(Util.KEY_PHONE_NUMBER, contact.getPhoneNumber());
+
+        db.insert(Util.TABLE_NAME, null, value);
+        db.close();
+    }
+
+    //Get a contact
+    public Contact getContact(int id){
+
+        SQLiteDatabase db = this.getReadableDatabase();
+        Cursor cursor = db.query(Util.TABLE_NAME, new String[] {Util.KEY_ID,
+                Util.KEY_NAME, Util.KEY_PHONE_NUMBER}, Util.KEY_ID + "=?",
+                new String[] {String.valueOf(id)}, null, null, null, null);
+
+        if (cursor != null)
+            cursor.moveToFirst();
+
+        Contact contact = new Contact(Integer.parseInt(cursor.getString(0)),
+                cursor.getString(1), cursor.getString(2));
+
+        return contact;
     }
 }
